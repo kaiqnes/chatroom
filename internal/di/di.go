b5fc5aba7caaebcfb977743fb8da41b498ced28f
Caique Nunes <caique.nunes@truepay.com.br>
2023-09-10 23:49:44 -0300

    inject initial dependencies

--- internal/di/di.go
+++ internal/di/di.go
@@ -2,19 +2,41 @@ package di
 
 import (
 	"chatroom/internal/config"
+	"chatroom/internal/controllers"
 	"chatroom/internal/db"
 	"chatroom/internal/logger"
+	"chatroom/internal/middlewares"
+	"github.com/gin-gonic/gin"
 )
 
 type di struct {
+	database *db.Database
+	engine   *gin.Engine
+	log      logger.CustomLogger
 }
 
-func New(cfg *config.Config, db *db.Database, logger logger.CustomLogger) (*di, error) {
+func New(cfg *config.Config, database *db.Database, engine *gin.Engine, log logger.CustomLogger) (*di, error) {
 	// Initialize dependencies
-	return &di{}, nil
+	return &di{
+		database: database,
+		engine:   engine,
+		log:      log,
+	}, nil
 }
 
 func (d *di) Inject() error {
-	// Inject dependencies
+	// Inject Middlewares
+	authMiddleware := middlewares.NewAuthenticationMiddleware(d.log)
+
+	// Inject Controllers
+	health := controllers.NewHealthCheckController(d.engine)
+	health.SetupEndpoints()
+
+	auth := controllers.NewAuthController(d.engine, authMiddleware)
+	auth.SetupEndpoints()
+
+	chatroom := controllers.NewChatroomController(d.engine, authMiddleware)
+	chatroom.SetupEndpoints()
+
 	return nil
 }
