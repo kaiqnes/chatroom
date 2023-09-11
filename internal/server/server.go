6e5f1e25c310c5e964db7fec2a205f8965d198d3
Caique Nunes <caique.nunes@truepay.com.br>
2023-09-11 12:52:07 -0300

    add cors

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,12 +2,14 @@ package server
 
 import (
 	"chatroom/internal/config"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func New(cfg *config.Config) (*gin.Engine, error) {
 	// Initialize server
 	engine := gin.Default()
+	engine.Use(cors.Default())
 
 	return engine, nil
 }
